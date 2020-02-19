65d2267e2cbc0311a2e5f5a90d761e6254a01ee8
EthanHolleman <eholleman@luc.edu>
2020-02-19 15:12:21 -0600

    edit data file so wget pulls to specific directory

--- data.py
+++ data.py
@@ -1,15 +1,16 @@
 import subprocess
 import os
 
-URLS = {'Donor_1_first': 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
-'Donor_1_after': 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
-'Donor_3 first': 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1',
-'Donor 3 after': 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1'}
+URLS = ['https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
+'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
+'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1',
+'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1']
 
 
-def run_wget(urls, output_dir):
+def run_wget(output_dir, urls=URLS):
     for url in urls:
-        cmd = ['wget', url, output_dir]
+        cmd = ['wget', url, '-O', os.path.join(output_dir, os.path.basename(url))]
+        print(' '.join(cmd))
         c = subprocess.call(cmd)
 
     return [os.path.join(output_dir, f) for f in os.listdir(output_dir)]
@@ -20,3 +21,5 @@ def convert_to_fastq(SRA_paths):
         subprocess.call(cmd)
         yield SRA_paths + '.fastq'
         #  yield the fastq file path
+
+run_wget('/home/ethan/Documents/github/test_dir')
