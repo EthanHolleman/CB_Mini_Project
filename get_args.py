9b776b05e253d9f7953f830933a5ea5b3018745d
EthanHolleman <eholleman@luc.edu>
2020-02-21 12:43:28 -0600

    forgot to include the get_args.py file in last commit

---
+++ get_args.py
@@ -0,0 +1,18 @@
+import argparse
+import sys
+
+
+def get_args():
+    parse = argparse.ArgumentParser(description='Process args for rm_verify')
+    parse.add_argument('-o', help='Output directory to write files to')
+    parse.add_argument('-i', help='Input directory if files already \
+                                  downloaded or for test data')
+    parse.add_argument('-k', help='Path to kallisto index if already created')
+    parse.add_argument(
+        '-q', help='Path to coverted fastq files if already created')
+    parse = parse.parse_args()
+
+    # output directory should be miniProject_Ethan_Holleman
+    # make the directory is already does not exist
+
+    return parse
