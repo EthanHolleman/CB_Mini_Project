3bc9b27df96ca16060fbf839185b6b204e26d6c9
EthanHolleman <eholleman@luc.edu>
2020-02-22 12:27:43 -0600

    Corrected urls in data.py

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ from Bio import SeqIO
 from Bio import SeqRecord
 from Bio.Seq import Seq
 
-URLS = ['https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
+URLS = ['https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660030/SRR5660030.1',
         'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
         'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1',
         'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1']
